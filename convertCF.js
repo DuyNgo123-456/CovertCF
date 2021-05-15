d753115fcd1109ce8f29fa780f57b125f7f60b95
unknown <you@example.com>
2021-05-15 17:42:08 +0700

    covertCF

---
+++ convertCF.js
@@ -0,0 +1,9 @@
+/*
+-b1:Đọc hiểu bài toán -> xác định input,ouput
+-b2:input (Độ C), output (Độ F), thuật toán Độ F = Độ C * 1.8 +32
+-b3:vẽ lưu đồ cho bài toán
+-b4:Code theo lưu đồ
+*/
+let DoC=+prompt('Nhap do C');
+DoF=(9/5)*DoC+32;
+alert ('Nhiet do F la:' + DoF + 'F')
